04f36ba03159a50cbd7a0844a9d33e39d9d7b6f0
Lereno <lereno.soares@gmail.com>
2023-09-02 19:28:46 -0300

    Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import markdownIntegration from '@astropub/md'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,7 +9,6 @@ export default defineConfig({
   integrations: [
     tailwind(), 
     sitemap(), 
-    mdx(),
-    markdownIntegration(),
+    mdx()
   ]
 });
